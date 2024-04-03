b47f6d24ae2dee34cbe52b19aff77a07e9384c9a
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-04-03 19:33:56 +0200

    Created simple program with 5 libraries.

---
+++ GoLang/zadania/homework2/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	srv := &http.Server{
+		Handler:      http.DefaultServeMux,
+		Addr:         "0.0.0.0:" + port,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(srv.ListenAndServe())
+}
